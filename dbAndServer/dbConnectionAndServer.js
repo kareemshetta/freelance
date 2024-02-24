b10c1591956804d1a6eba00709113aa7662ccb4f
Kareem Shetta <kareemshetta8@gmail.com>
2024-02-24 13:36:08 +0200

    remove-unneccessary-code

--- dbAndServer/dbConnectionAndServer.js
+++ dbAndServer/dbConnectionAndServer.js
@@ -4,15 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const dbConnectionAndServer = (app) => {
-  return mongoose
-    .connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("Connected successfully to DB");
-      const expressServer = app.listen(process.env.PORT || 5000, () =>
-        console.log(`Server listening on port ${process.env.PORT || 5000}`)
-      );
-    });
+  return mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("Connected successfully to DB");
+    const expressServer = app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server listening on port ${process.env.PORT || 5000}`)
+    );
+  });
 };
